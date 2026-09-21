c92b11b686574fbd38110e3eb4e8b0fe0d0fd0a3
Rodrigo <profrodrigo.sakai@fiap.com.br>
2026-09-21 21:15:50 +0000

    Databricks

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,20 +1,29 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.90"
     }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.83"
+    }
   }
+
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "sttfstatequeimadas"
-    container_name       = "tfstate"
-    key                  = "monitor-queimadas.tfstate"
+    container_name        = "tfstate"
+    key                   = "monitor-queimadas.tfstate"
   }
 }
 
-
 provider "azurerm" {
   features {}
+}
+
+provider "databricks" {
+  host = azurerm_databricks_workspace.this.workspace_url
 }
